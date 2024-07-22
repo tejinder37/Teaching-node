f5f8b9862f2b67982fb7fb6c81f6b52626a04c02
Tejinder Singh <tejinder94782@gmail.com>
2024-07-22 12:39:24 +0530

    comments added.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const { createServer } = require("node:http");
+//The createServer() method of http creates a new HTTP server and returns it.
 
+const { createServer } = require("node:http"); 
 const port = 3000;
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
+  res.statusCode = 200; //statusCode property to 200, to indicate a successful response
+  res.setHeader("Content-Type", "text/plain");//We set the Content-Type header: denoting type of response.
   let sayMyName = "Rohit";
-  res.end(`Hello node js , This is my first time . ${sayMyName}`);
+  res.end(`Hello node js , This is my first time . ${sayMyName}`);//adding the content as an argument to end():
 });
 
 server.listen(port, () => {
